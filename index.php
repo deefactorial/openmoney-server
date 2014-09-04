f139bf37be543ffe757dc04c2a5cedb08e74efb5
deefactorial <deefactorial@gmail.com>
2014-09-04 15:01:40 -0700

    return coded no decoded

--- index.php
+++ index.php
@@ -359,9 +359,10 @@ $app->post ( '/lookupTag', function () use($app) {
 			$tradingname_lookup_function = 
 			'function (doc, meta) { if( doc.type == "trading_name" && doc.steward && doc.name && doc.currency) { doc.steward.forEach(function( steward ) { emit( [steward, doc.currency, doc.name], { "name": doc.name, "currency": doc.currency } ); } ) } }';
 			
-			$designDoc =  json_decode( '{ "views": { "taglookup": { "map": "' . $taglookup_function . '" }, { "tradingnamelookup1" : { "map": "' . $tradingname_lookup_function . '" } } }' );
+			$designDoc = '{ "views": { "taglookup": { "map": "' . $taglookup_function . '" }, { "tradingnamelookup1" : { "map": "' . $tradingname_lookup_function . '" } } }';
 			
 			$cb->setDesignDoc( "dev_nfctag", $designDoc );
+			
 			
 			//echo $cb->getDesignDoc( "dev_nfctag" );
 			
