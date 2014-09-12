73f112d6fae668e77d925d93bb5f7f8a8c1e8396
deefactorial <deefactorial@gmail.com>
2014-09-11 21:52:33 -0700

    remove setting design doc

--- onchange.php
+++ onchange.php
@@ -1,13 +1,13 @@
 <?php 
 $cb = new Couchbase ( "127.0.0.1:8091", "openmoney", "", "openmoney" );
 
-$tradingNameJournal_lookup_function = 'function (doc, meta) { if( doc.type == \"trading_name_journal\" && doc.from && doc.to && doc.currency) { emit( \"trading_name,\" + doc.from + \",\" + doc.currency  ,  doc.from + \" \" + doc.currency); emit( \"trading_name,\" + doc.to + \",\" + doc.currency  ,  doc.to + \" \" + doc.currency); } }';
+// $tradingNameJournal_lookup_function = 'function (doc, meta) { if( doc.type == \"trading_name_journal\" && doc.from && doc.to && doc.currency) { emit( \"trading_name,\" + doc.from + \",\" + doc.currency  ,  doc.from + \" \" + doc.currency); emit( \"trading_name,\" + doc.to + \",\" + doc.currency  ,  doc.to + \" \" + doc.currency); } }';
 
-$designDoc = '{ "views": { "tradingnamejournallookup" : { "map": "' . $tradingNameJournal_lookup_function . '" } } }';
+// $designDoc = '{ "views": { "tradingnamejournallookup" : { "map": "' . $tradingNameJournal_lookup_function . '" } } }';
 	
-// echo $designDoc;
+// // echo $designDoc;
 	
-$cb->setDesignDoc ( "dev_roles", $designDoc );
+// $cb->setDesignDoc ( "dev_roles", $designDoc );
 	
 $options = array ('startkey' => array (), 'endkey' => array ( '\uefff'));
 	
