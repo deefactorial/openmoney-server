415a9d01dde508de1a67ce421a955b5031e0f255
deefactorial <deefactorial@gmail.com>
2014-11-19 11:41:10 -0800

    lets take a closer look at the results

--- onchange.php
+++ onchange.php
@@ -727,9 +727,9 @@ foreach ( $profiles ['rows'] as $profile ) {
 			//$options = array('startkey' => urlencode($profile_array['username']), 'endkey' => urlencode($profile_array['username']) . '\uefff');
 			$options = array();
 			$tradingname_result = $cb->view ( $design_doc_name, $stewardsTrading_name_function_name, $options );
-			//print_r( $tradingname_result );
+			print_r( $tradingname_result );
 			foreach ( $tradingname_result ['rows'] as $trading_name ) {
-				echo "key:" . $trading_name['key'] . " evals " . $trading_name['key'] == $profile_array['username'] . "\n";
+				//echo "key:" . $trading_name['key'] . " evals " . $trading_name['key'] == $profile_array['username'] . "\n";
 				if ($trading_name['key'] == $profile_array['username']) {
 					
 					//$trading_name = json_decode($trading_name['value'], true);
