b77b6934f69c4cc94fa5df9ce6a0ca5bf83a4e05
deefactorial <deefactorial@gmail.com>
2014-11-26 22:35:28 -0800

    add migration script

---
+++ migration.php
@@ -0,0 +1,64 @@
+<?php
+require ("config.php");
+
+function email_letter($to, $from, $subject = 'no subject', $msg = 'no msg') {
+	$headers = "From: $from\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+	$headers .= 'X-Mailer: PHP/' . phpversion ();
+	return mail ( $to, $subject, $msg, $headers );
+}
+
+$cb = new Couchbase ( "127.0.0.1:8091", "openmoney", "", "openmoney" );
+
+$tradingNameJournal_lookup_function = 'function(doc,meta){if(doc.type==\"trading_name_journal\"&&doc.from&&doc.to&&doc.currency){emit(\"trading_name,\"+doc.to+\",\" +doc.currency,doc.amount);}if(doc.type==\"trading_name_journal\"&&doc.from&&doc.to&&doc.currency){emit(\"trading_name,\"+doc.from+\",\"+doc.currency,-doc.amount);}}';
+
+$trading_name_journal_function_name = "tradingnamejournal";
+
+$designDoc = '{"views":{"' . $trading_name_journal_function_name . '":{"map":"' . $tradingNameJournal_lookup_function . '"}}}';
+
+// echo $designDoc;
+$design_doc_name = "dev_migration";
+
+$cb->setDesignDoc ($design_doc_name,json_decode(json_encode($designDoc)));
+
+$options = array ();
+
+// do trading name journal lookup
+$tradingnamejournal_result = $cb->view ( $design_doc_name, $trading_name_journal_function_name, $options );
+
+//print_r( $tradingnamejournal_result );
+foreach ( $tradingnamejournal_result ['rows'] as $journal_trading_name ) {
+	$trading_name_journal = json_decode( $cb->get( $journal_trading_name['id'] ), true );
+	$trading_name = json_decode( $cb->get ( $journal_trading_name['key'] ), true);
+	
+	if ($trading_name['name'] == $trading_name_journal['from']) {
+		foreach($trading_name['steward'] as $steward) {
+			$trading_name_view = json_decode( $cb->get("trading_name_view," . $steward . "," . $trading_name_journal['to'] . "," . $trading_name_journal['currency']) , true);
+			if (!isset($trading_name_view['trading_name'])) {
+				$trading_name_from_view = array();
+				$trading_name_from_view['type'] = "trading_name_view";
+				$trading_name_from_view['steward'] = array( $steward );
+				$trading_name_from_view['trading_name'] = $trading_name_journal['to'];
+				$trading_name_from_view['currency'] = $trading_name_journal['currency'];
+				$trading_name_from_view['created'] = intval( round(microtime(true) * 1000) );
+				$cb->set ("trading_name_view," . $trading_name_from_view['steward'] . "," . $trading_name_from_view['trading_name'] . "," . $trading_name_from_view['currency'] , json_encode ( $trading_name_from_view ) );
+			}
+		}
+	} else {
+		foreach($trading_name['steward'] as $steward) {
+			$trading_name_view = json_decode( $cb->get("trading_name_view," . $steward . "," . $trading_name_journal['from'] . "," . $trading_name_journal['currency']) , true);
+			if (!isset($trading_name_view['trading_name'])) {
+				$trading_name_from_view = array();
+				$trading_name_from_view['type'] = "trading_name_view";
+				$trading_name_from_view['steward'] = array( $steward );
+				$trading_name_from_view['trading_name'] = $trading_name_journal['from'];
+				$trading_name_from_view['currency'] = $trading_name_journal['currency'];
+				$trading_name_from_view['created'] = intval( round(microtime(true) * 1000) );
+				$cb->set ("trading_name_view," . $trading_name_from_view['steward'] . "," . $trading_name_from_view['trading_name'] . "," . $trading_name_from_view['currency'] , json_encode ( $trading_name_from_view ) );
+			}
+		}
+	}
+}
+
+
