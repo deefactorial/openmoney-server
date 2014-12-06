2d7fdb061b7e2d4c939666ab6048d4ff54fa6cbc
deefactorial <deefactorial@gmail.com>
2014-12-05 21:44:07 -0800

    nfc tags view

--- index.php
+++ index.php
@@ -876,6 +876,15 @@ $app->get ( '/openmoney_shadow/_design/dev_openmoney/_view/:viewname/', function
 			}
 			
 			
+		} else if ($viewname == 'nfc_tags') {
+			
+			$options = array ( array('startkey' => $username), array('endkey' => $username . '\uefff' ) ) ;
+			
+			$nfc_tags = $cb->view ( 'dev_openmoney', $viewname, $options );
+			
+			echo json_encode ($nfc_tags);
+			
+			
 		} else {
 			
 			
