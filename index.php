6216f904b9f45f40c2b6f68e65a159d430b85d77
deefactorial <deefactorial@gmail.com>
2015-01-07 16:13:31 -0800

    upate index

--- index.php
+++ index.php
@@ -830,9 +830,9 @@ $app->get ( '/openmoney_shadow/_design/dev_openmoney/_view/:viewname/', function
 		// do trading name lookup on
 		if ($viewname == 'accounts') {
 		
-			$options['stale'] = true;
+			//$options['stale'] = true;
 			$accounts = $cb->view ( 'dev_openmoney', $viewname, $options );
-			$options['stale'] = $stale;
+			//$options['stale'] = $stale;
 			
 			$tradingname_array = array ();
 			$tradingname_id_array = array ();
